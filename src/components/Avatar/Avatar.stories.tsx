caa37ef80b11451d8caab9ed028ebbed6302ebcd
falcon1 <sayfti@gmail.com>
2024-06-16 19:31:27 +0800

    task: grouped avatars

--- src/components/Avatar/Avatar.stories.tsx
+++ src/components/Avatar/Avatar.stories.tsx
@@ -1,5 +1,6 @@
 import { StoryFn, Meta } from "@storybook/react";
 import { Avatar, AvatarProps } from "./Avatar"
+import { AvatarGroup } from "@mui/material";
 
 export default {
   title: "Component/Avatar",
@@ -12,4 +13,17 @@ export const Default = Template.bind({});
 Default.args = {
   variant: "circular",
   size: "medium",
+};
+
+export const Grouped: StoryFn<AvatarProps> = (args) => (
+  <AvatarGroup max={4}>
+    <Avatar {...args}>A</Avatar>
+    <Avatar {...args}>B</Avatar>
+    <Avatar {...args}>C</Avatar>
+    <Avatar {...args}>D</Avatar>
+  </AvatarGroup>
+);
+Grouped.args = {
+  variant: "circular",
+  size: "small",
 };
